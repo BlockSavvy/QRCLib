4d212357adf462da61adff30a960a74291617e68
m@ <35651510+BlockSavvy@users.noreply.github.com>
2025-02-24 00:09:34 -0600

    fix: update path mappings for components

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,15 @@ const config = {
   output: 'standalone',
   experimental: {
     esmExternals: true
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': './app',
+      '@/lib': './lib',
+      '@/components': './components'
+    }
+    return config
   }
 }
 
